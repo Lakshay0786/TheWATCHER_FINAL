82475f611e279c09ab4fbe2a7230b4a2052542fb
Lakshay0786 <lakshay3899@gmail.com>
2023-04-10 11:31:32 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
+import { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+import { LoaderLarge } from './components/Loader';
+
+const VideoDetails = lazy(() => import('./components/VideoDetails'))
+const ChannelDetails = lazy(() => import('./components/ChannelDetail'))
+const SearchFeed = lazy(() => import('./components/SearchFeed'))
+const Feed = lazy(() => import('./components/Feed'))
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element:
+          <Suspense fallback={LoaderLarge} >
+            <Feed />
+          </Suspense >
+      },
+      {
+        path: "/video/:id",
+        element:
+          <Suspense fallback={LoaderLarge}>
+            <VideoDetails />,
+          </Suspense>
+
+      },
+      {
+        path: "/channel/:id",
+        element:
+          <Suspense fallback={LoaderLarge}>
+            <ChannelDetails />,
+          </Suspense>
+      },
+      {
+        path: "/search/:id",
+        element:
+          <Suspense fallback={LoaderLarge}>
+            <SearchFeed />,
+          </Suspense>
+      },
+    ]
+  }
+
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
